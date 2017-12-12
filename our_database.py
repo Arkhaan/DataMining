282049b9b91b6834445ed8631a26f24d518a476e
Jean Mainguy <jean.mainguy@outlook.fr>
2017-12-12 16:07:01 +0100

    fix header of the csv

--- our_database.py
+++ our_database.py
@@ -13,7 +13,7 @@ fact_table.write("ID_Prot;Organism\n")
 organisms = ["Drosophila", "Homo", "Arabidopsis", "Maize"]
 count = -1
 for file in json_files:
-    
+
     fl = open(file, 'r')
 
     print ("\n" + "Reading " + file + "\n")
@@ -22,59 +22,59 @@ for file in json_files:
     parsed_json = json.loads(line)
 
     count += 1
-    
+
     for k in parsed_json:
         proteins[k] = {}
         my_seq = parsed_json[k]["seq"]
         proteins[k]["seq"] = my_seq
 
         #On entre la proteine dans la table de faits :
-        
+
         fact_table.write(k + "\n")
 
         proteins[k]["organism"] = organisms[count]
-        
-        
+
+
         analysed_seq = ProteinAnalysis(my_seq)
         try :
             proteins[k]["weight"] = analysed_seq.molecular_weight()
         except :
-            proteins[k]["weight"] = None
+            proteins[k]["weight"] = "NA"
             print "pas de weight"
         try :
             proteins[k]["size"] = len(my_seq)
         except :
-            proteins[k]["size"] = None
-            print "pas de weight" 
+            proteins[k]["size"] = "NA"
+            print "pas de weight"
         try :
             proteins[k]["isoelectric point"] = analysed_seq.isoelectric_point()
         except :
-            proteins[k]["isoelectric point"] = None
-            print "pas de isoelectric point" 
+            proteins[k]["isoelectric point"] = "NA"
+            print "pas de isoelectric point"
         try :
             proteins[k]["aromaticity"] = analysed_seq.aromaticity()
         except :
-            proteins[k]["aromaticity"] = None
-            print "pas de aromaticity" 
+            proteins[k]["aromaticity"] = "NA"
+            print "pas de aromaticity"
         try :
             proteins[k]["instability index"] = analysed_seq.instability_index()
         except :
-            proteins[k]["instability index"] = None
+            proteins[k]["instability index"] = "NA"
             print "pas de instability index"
         try :
             secondary = analysed_seq.secondary_structure_fraction()
             proteins[k]["helix"] = secondary[0]
             proteins[k]["turn"] = secondary[1]
             proteins[k]["sheet"] = secondary[2]
-            
+
         except :
-            proteins[k]["helix"] = None
-            proteins[k]["turn"] = None
-            proteins[k]["sheet"] = None
+            proteins[k]["helix"] = "NA"
+            proteins[k]["turn"] = "NA"
+            proteins[k]["sheet"] = "NA"
             print "pas de secondary structures"
 
 csv =open("sequence_table.csv", "w")
-csv.write("id_protein;seq;weight;isoelectric point;aromaticity;instability index;helix;turn;sheet\n")
+csv.write("id_protein;organism;seq;weight;size;isoelectric point;aromaticity;instability index;helix;turn;sheet\n")
 for key in proteins.keys():
     id_protein = key
     organism = proteins[key]["organism"]
